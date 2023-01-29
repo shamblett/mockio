ae0d7a8179b623d497a8ba89b553a9d83b250409
shamblett <steve.hamblett@linux.com>
2023-01-29 15:07:29 +0000

    Initial create

---
+++ lib/mockio.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mockio;
+
+export 'src/mockio_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
